aa265beffad359b4ce8b867b7a8231274aaee1b8
samuil22 <samiulsabbir8262@gmail.com>
2020-03-09 17:44:38 +0600

    firstov2

---
+++ main.js
@@ -0,0 +1,29 @@
+
+// Different effects for showing and closing modal
+let fadeIn = 'slideInRight';
+let fadeOut = 'slideOutRight';
+
+// On show
+$('#myModal').on('show.bs.modal', function () {
+$(this).removeClass(fadeOut);
+$(this).addClass(fadeIn);
+});
+
+// On closing
+$('#myModal').on('hide.bs.modal', function (e) {
+let $this = $(this);
+
+// Check whether the fade in class still exists in this modal
+// if this class is still exists prevent this modal
+// to close immediately by setting up timeout, and replace
+// the fade in class with fade out class.
+if ($this.hasClass(fadeIn)) {
+    $this.removeClass(fadeIn);
+    $this.addClass(fadeOut);
+    e.preventDefault();
+
+    setTimeout(function () {
+        $this.modal('hide');
+    }, 1000); // the default delays from animate.css is 1s
+}
+});
